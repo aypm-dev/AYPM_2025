2f89926774ba03ae3d76b4ca76df4914ce7c3a7d
AngelCode-dev <angelpaniagua2004@gmail.com>
2023-02-06 01:58:44 -0600

    feat: About page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,34 @@
+<div class="relative flex flex-col w-full scale-90 sm:scale-100 items-center text-eerie-900">
+
+    <div class="flex w-full mt-32">
+        <div class="flex flex-col h-fit w-full p-12 items-center text-snow border-t-[3px] border-b-[6px] border-eerie-900 bg-folly">
+            <h1 class="text-center text-6xl"> ABOUT </h1>
+
+            <p class="mt-4 text-xl text-center"> 
+                Me, Angel!! I am a software developer, I recently completed my "Bachillerato" in Networking, and Im persuing
+                a career in Web Developer, 
+            </p>
+
+            <span class="w-[46rem] mt-32">
+                Hey! Hover down here <i class="fa-solid fa-turn-down mr-2"/>
+            </span>
+            <div class="flex p-4 px-20 w-fit gap-4 justify-evenly rounded-xl border-[3px] border-b-[6px] text-eerie-900 border-eerie-900 shadow-lg bg-white">
+                <div class="relative flex w-32 max-h-32 justify-center items-center">
+                    <img style="filter: drop-shadow(0 0.35rem 0.2px black)" src="svelte-logo.png" loading="lazy" alt="Svelte Logo" class="animate-inverse-half-disappearing h-32">
+                    <img style="filter: drop-shadow(0 0.35rem 0.2px black)" src="sveltekit-logo.png" loading="lazy" alt="Svelte Logo" class="animate-half-disappearing absolute">
+                </div>
+                <div class="flex w-32 h-32 justify-center items-center">
+                    <img style="filter: drop-shadow(0 0.35rem 0.2px black)" src="react-logo.png"loading="lazy" alt="Svelte Logo">
+                </div>
+                <div class="flex w-32 h-32 justify-center items-center">
+                    <img style="filter: drop-shadow(0 0.35rem 0.2px black)" src="css-logo.png" loading="lazy" alt="Svelte Logo" class="animate-inverse-half-disappearing absolute h-32">
+                    <img style="filter: drop-shadow(0 0.35rem 0.2px black)" src="tailwind-logo.png" loading="lazy" alt="Svelte Logo" class="animate-half-disappearing h-32">
+                </div>
+                <div class="relative flex w-32 h-32 justify-center items-center">
+                    <img style="filter: drop-shadow(0 0.35rem 0.2px black)" src="javascript-logo.png" loading="lazy" alt="Svelte Logo" class="rounded-xl">
+                    <img style="filter: drop-shadow(0 0.35rem 0.2px black)" src="typescript-logo.png" loading="lazy" alt="Svelte Logo" class="animate-half-disappearing absolute rounded-xl">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,10 +7,26 @@ module.exports = {
         "infinite-scroll": {
           'from': { transform: "translateY(0%)" },
           'to': { transform: 'translateY(-50%)' },
+        },
+        "half-disappearing": {
+          '0%': { opacity: "0%" },
+          '12.5%': { opacity: "100%" },
+          '50%': { opacity: "100%" },
+          '87.5%': { opacity: "0%" },
+          '100%': { opacity: "0%" },
+        },
+        "inverse-half-disappearing": {
+          '0%': { opacity: "100%" },
+          '12.5%': { opacity: "0%" },
+          '50%': { opacity: "0%" },
+          '87.5%': { opacity: "100%" },
+          '100%': { opacity: "100%" },
         }
       },
       animation: {
-        'infinite-scroll': 'infinite-scroll 10S linear infinite'
+        'infinite-scroll': 'infinite-scroll 10S linear infinite',
+        'half-disappearing': 'half-disappearing 15S linear infinite forwards',
+        'inverse-half-disappearing': 'inverse-half-disappearing 15S linear infinite forwards'
       },
       colors: {
         'eerie': {
