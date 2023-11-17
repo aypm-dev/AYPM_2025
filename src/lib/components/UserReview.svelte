21bce2c79a6c3f7cd296df26205c383ac7b00978
AngelCode-dev <angelpaniagua2004@gmail.com>
2023-11-17 00:31:19 -0600

    feat: angry face on empty review

--- src/lib/components/UserReview.svelte
+++ src/lib/components/UserReview.svelte
@@ -9,8 +9,7 @@
     <div class="flex flex-col items-start overflow-clip">
         <h1 class="text-3xl"> {user_name}: </h1>
         <p class="flex text-xl sm:whitespace-nowrap items-center"> 
-            {text? text: "I hate empty strings"}
+            {text? text: "I hate empty strings >:("}
         </p>
     </div>
 </div>
-
