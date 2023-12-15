2eb400e19c9cbcf29b51204d5d380cd8b8eab8e9
AngelCode-dev <angelpaniagua2004@gmail.com>
2023-12-15 01:49:14 -0600

    feat: hcbs branding in HCBS modal

--- src/routes/about/projects/HCBS.svelte
+++ src/routes/about/projects/HCBS.svelte
@@ -3,8 +3,15 @@
 </script>
 
 <div style="grid-template-columns: 3fr 7fr" class="grid w-full gap-4">
-    <div class="flex flex-col w-FIT h-full mt-12 items-end">
+    <div class="hidden sm:flex flex-col w-FIT h-full mt-12 items-end">
         {#each {length: 12} as _, index}
+            <h1 class="-mt-[39.5px] sm:-mt-10 text-6xl sm:text-7xl whitespace-nowrap"> HCBS? </h1>
+        {/each}
+    </div>
+
+
+    <div class="sm:hidden flex flex-col w-FIT h-full mt-12 items-end">
+        {#each {length: 18} as _, index}
             <h1 class="-mt-[39.5px] sm:-mt-10 text-6xl sm:text-7xl whitespace-nowrap"> HCBS? </h1>
         {/each}
     </div>
@@ -16,28 +23,45 @@
             Don't you get this burst of motivation wanting to create something in the middle of the night? Well, the same happened to me...
         </p>
         <p class="hidden sm:block text-xl text-justify">
-            Watching other's amazing 3D renders created a desire deep inside of me to get something cool out there! 
-            Next thing in the morning in my game engine search I found <a href="godotengine.org/">Godot</a>, and how the kids say these days, it was love a first sigth! (do kids even say that? mmh..)<br><br>
+            Watching amazing 3D renders created a desire deep inside of me to get something cool out there! 
+            Next thing in the morning in my game engine search I found <a href="https://godotengine.org/" target="_blank" rel="noreferrer" class="underline">Godot</a>, and how the kids say these days, it was love a first sigth! (do kids even say that? mmh..)<br><br>
         </p>
             
         <p class="hidden sm:block text-xl text-justify">
-            Anyways, after the classic first project to learn the engine it was time for the good stuff. What about a 30 day project ;)
+            Anyways, after the classic first project to learn the engine it was time for the good stuff. What about a 30 day project ;)?
         </p>
 
     </div>
 </div>
 
+<p class="sm:hidden block mt-4 text-xl text-justify">
+    Watching amazing 3D renders created a desire deep inside of me to get something cool out there! 
+    Next thing in the morning in my game engine search I found <a href="https://godotengine.org/" target="_blank" rel="noreferrer"  class="underline">Godot</a>, and how the kids say these days, it was love a first sigth! (do kids even say that? mmh..)<br><br>
+</p>
+    
+<p class="sm:hidden block text-xl text-justify">
+    Anyways, after the classic first project to learn the engine it was time for the good stuff. What about a 30 day project ;)?
+</p>
+
+<img src="hcbs-logo.png" alt="Hero kiwi" class="mx-auto w-64 mt-12 mb-12">
+
+
 <p class="sm:w-2/3 mx-auto mt-12 text-xl text-justify"> 
-    That's what Heroes Can Be Selfish is, a game I started, programmed, and published in only a month out of that inspiration!. This includes the code base, all artwork, game page, publicity, testing, and trailers!
+    That's what Heroes Can Be Selfish is, a game I programmed, and published in only a month out of that inspiration!. This includes the code base, all artwork, game page, publicity, testing, and trailers!
 </p>
 
 <HeroGame/>
 
-<p class="sm:w-2/3 mx-auto mt-12 mb-24 text-xl text-justify"> 
+<p class="sm:w-2/3 mx-auto mt-12  text-xl text-justify"> 
     In every story the heroes are the embodyment of the selfless persona looking for the well-being of the people, saving them from mosters and returning back treasures from generations past... <br><br>
+
+
     But what if the hero of this tale could take advantage of his heritage from a family of brave breed kiwis? And when no one in town suppects, their loose coins could be stolen out of their pockets by our kiwi... a broke and kleptomaniac kiwi addicted to purchasing useless items on Amezon to fill his home ;0
 </p>
 
-
+<div class="flex">
+    <a href="https://joskgames.itch.io/heroes-can-be-selfish" target="_blank" rel="noreferrer"  class="underline text-2xl mx-auto text-center mt-8 ">Heroes Can Be Selfish</a>
+</div>
+<img src="josk.png" alt="Hero kiwi" class="mx-auto w-24 mt-4 mb-24">
 
 
--- src/routes/about/projects/HeroGame.svelte
+++ src/routes/about/projects/HeroGame.svelte
@@ -3,8 +3,14 @@
 </script>
 
 <div class="flex w-full mt-8 gap-2 items-center justify-center rounded-xl border-[3px] border-b-[6px] border-eerie-900 overflow-x-hidden ring-white ring-2">
+    <h3 class="text-5xl font-bold mr-4 opacity-25">
+        HC
+    </h3>
     <img src="hcbs-big-coin.gif" alt="Hero kiwi" class="w-12 mt-4">
     <img src="hcbs.gif" alt="Hero kiwi" class="w-12 mt-8 -scale-x-100">
+    <h3 class="text-5xl font-bold ml-4 opacity-25">
+        BS
+    </h3>
 </div>
 
 
