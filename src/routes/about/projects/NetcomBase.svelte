855c703246df55a5e7045c127e8d21d9a316aa0d
AngelCode-dev <angelpaniagua2004@gmail.com>
2023-12-14 23:11:16 -0600

    feat: netcomBaseModal

---
+++ src/routes/about/projects/NetcomBase.svelte
@@ -0,0 +1,31 @@
+<div style="grid-template-columns: 3fr 7fr" class="grid w-full gap-4">
+    <div class="flex flex-col w-FIT h-full mt-12 items-end">
+        {#each {length: 13} as _, index}
+            <h1 class="-mt-[40px] sm:-mt-10 text-6xl sm:text-7xl whitespace-nowrap"> CRM? </h1>
+        {/each}
+    </div>
+
+    <div class="flex flex-col items-center">
+        <span class="mt-4 text-2xl font-bold"> NETCOM BASE? </span>
+
+        <p class="mt-4 text-xl text-justify"> 
+            Well looks like the <b>C</b>ustomer <b>R</b>elationship <b>M</b>anagemer our company, Lionix, made for AYA (Institution in charge of infrastructure of water supply in Costa Rica) got it's scope outgrown with the years due to the amount of trafic and users that got into the project. <br>
+            Time for an Upgrade! And I was in charge from the start. This improvement has to resist the passage of time with cleaner and modular code <br><br>
+        </p>
+
+        <p class="hidden sm:block text-xl text-justify">
+            And the modular part of the code was super important, because not only it was an update, but a whole template for future <b>CRM</b>s for Netcom a business partner of Lionix :)
+        </p>
+    </div>
+</div>
+
+<p class="sm:w-2/3 mx-auto mt-12 text-xl text-justify"> 
+    CRM Netcom Base was called. And its job did! As of today, the base template was used in 4 projects and hopefully more in the future. One of this projects was a showcase for a bank's future delivering of cards system for the entire country, this really put the capabilities of the project due to the short deadline of only 2 WEEKS!! (Lots of extra hours...) The project was later developed and put together into the final version in another 3 weeks.
+</p>
+
+
+<p class="sm:w-2/3 mx-auto mt-12 mb-24 text-xl text-justify"> 
+    After a few months of solo development my good teammate joined the project and we managed to split the project perfectly so we could always have work to do.<br>
+    This big project made with Svelte really put my skill to the test and 
+</p>
+
