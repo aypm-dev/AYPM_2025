da5396b24e4ba856e9eb807f15589b5cb1bc4dff
AYPM <85044270+aypm-dev@users.noreply.github.com>
2024-08-22 16:53:08 -0600

    feat: page links in footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,10 +1,14 @@
 <footer class="text-center">
-	<hr class="mx-12 mb-2 rounded-xl border-b-[3px] border-eerie-900">
-	<p>
-		Fron-end developer, especialiced in Web Apps ® 2023 KIWICOLA™ - AYPM
-	</p>
+	<hr class="mx-12 mb-2 rounded-xl border-b-[3px] border-eerie-900" />
+
+	;) Aditional Links:
+	<div class="flex gap-4 justify-center">
+		<a href="/about" class="underline">About Angel Paniagua Mora</a> |
+		<a href="/contact" class="underline">AYPM Contact form</a>
+	</div>
+
+	<p>Fron-end developer, especialiced in Web Apps ® 2023 KIWICOLA™ - AYPM</p>
 	<p class="mt-4">
-		Contact angelpaniagua2004@gmail.com <br> Angel Yader Paniagua Mora™
-		All Rigths Reserved.
+		Contact angelpaniagua2004@gmail.com <br /> Angel Yader Paniagua Mora™ All Rigths Reserved.
 	</p>
 </footer>
