169c69799cfbed6f55e1c9e71791f5ad0cd17bd0
AYPM <85044270+aypm-dev@users.noreply.github.com>
2024-10-07 02:57:27 -0600

    feat: lower resolution favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -23,13 +23,7 @@
 
 <svelte:head>
 	<meta charset="UTF-8" />
-	<link rel="icon" href="/favicon.png" sizes="32x32" type="image/png" />
-	<link rel="icon" href="/favicon.png" sizes="16x16" type="image/png" />
 
-	<meta
-		name="description"
-		content="Explore the portfolio of Angel Paniagua Mora, filled with projects I worked in before, and important info."
-	/>
 	<meta
 		name="keywords"
 		content="Angel Paniagua Mora, webdev, aypm, AYPM portfolio, portfolio, frontend developer, developer, frontend, backend, backend developer, aypm dev, dev, costa rica desarrollador"
@@ -37,15 +31,15 @@
 	<meta name="author" content="Angel Paniagua Mora" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <meta property="og:site_name" content="<AYPM /> Frontend Developer Portfolio"/>
+	<meta property="og:site_name" content="<AYPM /> Frontend Developer Portfolio" />
 	<meta property="og:title" content="Angel Paniagua Mora | Full-Stack Developer Portfolio" />
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content="https://aypm.dev" />
 	<meta property="og:image" content="https://www.aypm.dev/hcbs.png" />
 
-    <meta name="twitter:card" content="summary"/>
-    <meta name="twitter:title" content="<AYPM /> Frontend Developer Portfolio"/>
-    <meta name="twitter:description" content="About Angel Paniagua Mora, and contact info/methods">
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:title" content="<AYPM /> Frontend Developer Portfolio" />
+	<meta name="twitter:description" content="About Angel Paniagua Mora, and contact info/methods" />
 
 	<script type="application/ld+json">
         {
